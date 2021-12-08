006f6176457246de8a307a244388afd508e7fd1c
Temirov Ilkom <ilhomtemirov9601@gmail.com>
2021-12-09 03:44:03 +0900

    JS

---
+++ index.js
@@ -0,0 +1,92 @@
+//  ******** JS IF / IF ELSE / ELSE ******
+// function ajrat(a) {
+//     if (a % 2 == 0) {
+//         console.log(`${a} son juft son`);
+//     } else {
+//         console.log(`${a} son toq son`);
+//     }
+// }
+
+// ajrat(5);
+
+
+// function nimadir(a, b) {
+//     // if (2 + 2 == 4) {
+//     //     else (){
+
+//     //     }
+//     // }
+//     if (a + b == 4) {
+//         console.log("ha");
+//     } else {
+//         console.log('yuq');
+//     }
+// }
+
+// nimadir(2, 2);
+
+// function loginOut(username) {
+//     if (username == "ilhom") {
+//         console.log(`Xush kelibsiz ${username}`)
+//     } else if (username == "Jonibek") {
+//         console.log(`Xush kelibsiz ${username}`)
+//     } else if (username == "Bekzod") {
+//         console.log(`Xush kelibsiz ${username}`)
+//     } else if (username == "Ahmad") {
+//         console.log(`Xush kelibsiz ${username}`)
+//     } else {
+//         console.log(`${username} bunaqa user mavjud emas!`)
+//     }
+// }
+
+// loginOut("sardor");
+
+
+//  ******** JS OBJECT ******
+// var odam = {
+//     ism: "Ilhom",
+//     yosh: 26,
+//     manzil: "denav",
+//     uylanganmi: false
+// }
+
+// console.log(odam)
+
+// function uylan(qiz) {
+//     if (qiz == true) {
+//         console.log("uylangan");
+//     } else {
+//         console.log("uylanmagan");
+//     }
+// }
+// uylan(odam.uylanganmi);
+
+
+
+// function name(user) {
+//     if (user != "jonik") {
+//         console.log("false")
+//     } else {
+//         console.log("true")
+//     }
+// }
+
+// name("ilhom")
+
+
+// JS ARIFMETIK SHORT
+// var a = 1;
+// var b = 2;
+// a += b // a = a + b
+// a -= b; // a = a - b
+// a *= b; // a = a * b
+// a /= b; // a = a / b
+// a %= b; // a a % b
+// console.log(a);
+
+
+// ****** JS LISTS ******
+// var ismlar = ["ilhom", "Jonik", "Bekzod", ];
+// console.log(ismlar[4]);
+
+// console.log(ismlar.length) // Elementlarni sonini aniqlash
